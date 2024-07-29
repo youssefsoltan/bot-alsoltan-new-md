514bf93c27d55c464dfa645988fff88c79bf70bc
SAYED_SHAWAZA <shmdy7938@gmail.com>
2024-07-29 07:48:13 +0300

    Update المالك-تصليح.js

--- plugins/المالك-تصليح.js
+++ plugins/المالك-تصليح.js
@@ -1,4 +1,45 @@
-/* Codigo hecho por @Fabri115 y mejorado por BrunoSobrino */
+import { readdirSync, unlinkSync, existsSync, promises as fs, rmSync, readFileSync } from 'fs';
+import path from 'path';
+
+const handler = async (m, { conn, usedPrefix }) => {
+  
+
+  if (global.conn.user.jid !== conn.user.jid) {
+    return conn.sendMessage(m.chat, {text: "عذراً، لا يمكنك استخدام هذا الأمر من هذا الجهاز."}, {quoted: m});
+  }
+  const chatId = m.isGroup ? [m.chat, m.sender] : [m.sender];
+  const sessionPath = './MysticSession/';
+  try {
+    const files = await fs.readdir(sessionPath);
+    let filesDeleted = 0;
+    for (const file of files) {
+      for (const id of chatId) {
+        if (file.includes(id.split('@')[0])) {
+          await fs.unlink(path.join(sessionPath, file));
+          filesDeleted++;
+          break;
+        }
+      }
+    }
+    if (filesDeleted === 0) {
+      await conn.sendMessage(m.chat, {text: "لم يتم العثور على أي ملفات لحذفها 🧞."}, {quoted: m});
+    } else {
+      await conn.sendMessage(m.chat, {text: `تم حذف ${filesDeleted} ملف بنجاح 🧞.`}, {quoted: m});
+    }
+  } catch (err) {
+    console.error("حدث خطأ أثناء محاولة حذف الملفات:", err);
+    await conn.sendMessage(m.chat, {text: "حدث خطأ أثناء حذف الملفات 🧞."}, {quoted: m});
+  }
+  await conn.sendMessage(m.chat, {text: `تمت العملية بنجاح 🧞.`}, {quoted: m});
+};
+handler.help = ['csowner'];
+handler.tags = ['owner'];
+handler.command = /^(تصليح|cs)$/i;
+handler.rowner = true;
+export default handler;
+
+
+/*
 
 import { readdirSync, unlinkSync, existsSync, promises as fs, rmSync, readFileSync } from 'fs';
 import path from 'path';
@@ -42,3 +83,4 @@ handler.tags = ['owner'];
 handler.command = /^(تصليح|cs)$/i;
 handler.rowner = true;
 export default handler;
+*/
