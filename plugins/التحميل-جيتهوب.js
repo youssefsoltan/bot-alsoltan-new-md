d6e22fb9cdcebeb6bfcf30da1e45ffcc3171d2a0
SAYED_SHAWAZA <shmdy7938@gmail.com>
2024-07-29 03:02:32 +0300

    Update التحميل-جيتهوب.js

--- plugins/التحميل-جيتهوب.js
+++ plugins/التحميل-جيتهوب.js
@@ -1,29 +1,4 @@
-import fetch from 'node-fetch';
-import fs from 'fs';
 
-const regex = /(?:https|git)(?::\/\/|@)github\.com[\/:]([^\/:]+)\/(.+)/i;
-const handler = async (m, { args, usedPrefix, command }) => {
-  const datas = global;
-  const idioma = datas.db.data.users[m.sender].language;
-  const _translate = JSON.parse(fs.readFileSync(`./language/${idioma}.json`));
-
-  if (!args[0]) throw `يرجى تقديم رابط GitHub صحيح باستخدام الصيغة التالية: _${usedPrefix + command} https://github.com/BrunoSobrino/TheMystic-Bot-MD_`;
-  if (!regex.test(args[0])) throw `رابط GitHub غير صحيح. يرجى التحقق من الرابط وإعادة المحاولة.`;
-  let [_, user, repo] = args[0].match(regex) || [];
-  repo = repo.replace(/.git$/, '');
-  const url = `https://api.github.com/repos/${user}/${repo}/zipball`;
-  const filename = (await fetch(url, { method: 'HEAD' })).headers.get('content-disposition').match(/attachment; filename=(.*)/)[1];
-  
-  m.reply('جاري تحميل الملف من GitHub...');
-  conn.sendFile(m.chat, url, filename, null, m);
-};
-
-handler.command = /gitclone|repo|جيتهوب/i;
-export default handler;
-
-
-
-/*
 import fetch from 'node-fetch';
 
 const regex = /(?:https|git)(?::\/\/|@)github\.com[\/:]([^\/:]+)\/(.+)/i;
@@ -33,15 +8,14 @@ const handler = async (m, {args, usedPrefix, command}) => {
   const _translate = JSON.parse(fs.readFileSync(`./language/${idioma}.json`))
   const tradutor = _translate.plugins.descargas_gitclone
 
-  if (!args[0]) throw `${tradutor.texto1} _${usedPrefix + command} https://github.com/BrunoSobrino/TheMystic-Bot-MD_`;
-  if (!regex.test(args[0])) throw tradutor.texto2;
+  if (!args[0]) throw `يرجى تقديم رابط GitHub صحيح باستخدام الصيغة التالية: *${usedPrefix + command}* https://github.com/BrunoSobrino/TheMystic-Bot-MD`;
+  if (!regex.test(args[0])) throw `رابط GitHub غير صحيح. يرجى التحقق من الرابط وإعادة المحاولة.`;
   let [_, user, repo] = args[0].match(regex) || [];
   repo = repo.replace(/.git$/, '');
   const url = `https://api.github.com/repos/${user}/${repo}/zipball`;
   const filename = (await fetch(url, {method: 'HEAD'})).headers.get('content-disposition').match(/attachment; filename=(.*)/)[1];
-  m.reply(tradutor.texto3);
+  m.reply('جاري تحميل الملف من GitHub...');
   conn.sendFile(m.chat, url, filename, null, m);
 };
-handler.command = /gitclone/i;
+handler.command = /gitclone|repo|جيتهوب/i;
 export default handler;
-*/
