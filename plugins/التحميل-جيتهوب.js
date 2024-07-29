2fcc8902752f6a3d69112ed84514021178982d9c
SAYED_SHAWAZA <shmdy7938@gmail.com>
2024-07-29 08:34:34 +0300

    Update التحميل-جيتهوب.js

--- plugins/التحميل-جيتهوب.js
+++ plugins/التحميل-جيتهوب.js
@@ -17,5 +17,5 @@ const handler = async (m, {args, usedPrefix, command}) => {
   m.reply('جاري تحميل الملف من GitHub...');
   conn.sendFile(m.chat, url, filename, null, m);
 };
-handler.command = /gitclone|repo|جيتهوب/i;
+handler.command = /gitclone|repo|جيت/i;
 export default handler;
