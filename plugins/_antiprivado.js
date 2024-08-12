84904757c291a788ff5c4d13ac59f366f3bf8e60
SAYED_SHAWAZA <shmdy7938@gmail.com>
2024-08-13 01:56:11 +0300

    Update _antiprivado.js

--- plugins/_antiprivado.js
+++ plugins/_antiprivado.js
@@ -10,6 +10,10 @@ export async function before(m, {conn, isAdmin, isBotAdmin, isOwner, isROwner}) 
     const _translate = JSON.parse(fs.readFileSync(`./language/${idioma}.json`))
     const tradutor = _translate.plugins._antiprivado
 
+const cap = `رساله للجميع  :
+بصفتي  مطور  البوت اود  اخباركم  جميعا انه  يحظر  استخدام البوت  في الخاص وانه  يسمح فقط  باستخدام البوت من  خلال هذه المجموعه  فقط..
+https://chat.whatsapp.com/BzsALGDPK3sAsK2Y3m6zJY`;
+   
   if (m.isBaileys && m.fromMe) return !0;
   if (m.isGroup) return !1;
   if (!m.message) return !0;
@@ -17,7 +21,7 @@ export async function before(m, {conn, isAdmin, isBotAdmin, isOwner, isROwner}) 
   const chat = global.db.data.chats[m.chat];
   const bot = global.db.data.settings[this.user.jid] || {};
   if (bot.antiPrivate && !isOwner && !isROwner) {
-    await m.reply(tradutor.texto1, false, {mentions: [m.sender]});
+    await m.reply(cap, false, {mentions: [m.sender]});
     await this.updateBlockStatus(m.chat, 'block');
   }
   return !1;
